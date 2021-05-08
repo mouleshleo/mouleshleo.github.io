5c4d5bf725d383d4822aed83f9e983f148bb97ca
Moulesh <mouleshleo06@gmail.com>
2021-05-08 12:10:38 +0530

    search was improved

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert('hello world');
+function search() {
+  var x = document.getElementById("searchbar").value;
+  if (x == "") {
+    alert("Please enter your name.");
+  } else {
+    alert(
+      "Hi " +
+        x +
+        ",\r\n" +
+        "this page is created for fun. more features are coming soon.\r\n-By Moulesh."
+    );
+  }
+}
