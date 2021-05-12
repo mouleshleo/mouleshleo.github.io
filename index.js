85cae8ada13d373303ccf92151e8b84a5a5bbb0f
Moulesh <mouleshleo06@gmail.com>
2021-05-12 11:26:20 +0530

    new feature added

--- index.js
+++ index.js
@@ -21,6 +21,42 @@ function changeColor() {
   change.style.border = "1px solid rgb(0, 132, 255)";
 }
 
+
+function onSuccess(googleUser) {
+  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+  console.log(error);
+}
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // page view counter
 
 const countEl = document.getElementById('count');
