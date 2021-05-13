9ac0cff137a2c40e22d3f9398229f613d3537d90
Moulesh <mouleshleo06@gmail.com>
2021-05-13 12:53:36 +0530

    xml file added

--- index.js
+++ index.js
@@ -39,3 +39,10 @@ function renderButton() {
     'onfailure': onFailure
   });
 }
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
