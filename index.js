cfec7c690b1078ad77cf03398dc3c78ccbea1507
Moulesh <mouleshleo06@gmail.com>
2021-05-15 15:31:32 +0530

    preloader added

--- index.js
+++ index.js
@@ -21,28 +21,61 @@ function changeColor() {
   change.style.border = "1px solid rgb(0, 132, 255)";
 }
 
-
 function onSuccess(googleUser) {
-  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  console.log("Logged in as: " + googleUser.getBasicProfile().getName());
 }
 function onFailure(error) {
   console.log(error);
 }
 function renderButton() {
-  gapi.signin2.render('my-signin2', {
-    'scope': 'profile email',
-    'width': 240,
-    'height': 50,
-    'longtitle': true,
-    'theme': 'dark',
-    'onsuccess': onSuccess,
-    'onfailure': onFailure
+  gapi.signin2.render("my-signin2", {
+    scope: "profile email",
+    width: 180,
+    height: 25,
+    longtitle: true,
+    theme: "dark",
+    onsuccess: onSuccess,
+    onfailure: onFailure,
   });
 }
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
-    console.log('User signed out.');
+    console.log("User signed out.");
   });
 }
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log("Name: " + profile.getName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function blog() {
+  alert("blog is coming soon");
+}
+
+var loader;
+function loadNow(opacity) {
+  if (opacity <= 0) {
+    displayContent();
+  } else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function () {
+      loadNow(opacity - 0.05);
+    }, 90);
+  }
+}
+
+function displayContent() {
+  loader.style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  loader = document.getElementById("loader");
+  loadNow(1);
+});
